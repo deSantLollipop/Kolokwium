fcd6952b9cae83013344af7ccf86fd7ae2b19fe5
Ivan <35543935+deSantLollipop@users.noreply.github.com>
2020-06-15 12:10:04 +0200

    Add files via upload

---
+++ Zadanie_1/Zadanie_1/Decorator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    //component
+    public abstract class Kolokwium
+    {
+        public abstract decimal Ocena();
+    }
+
+    //concrete components
+    class MojKolokwium : Kolokwium
+    {
+        public override decimal Ocena()
+        {
+            return 1M;
+        }
+    }
+
+    //dekorator
+    abstract class KolokwiumzZadaniami : Kolokwium
+    {
+        protected Kolokwium kolokwium;
+
+        public KolokwiumzZadaniami(Kolokwium kolokwium)
+        {
+            this.kolokwium = kolokwium;
+        }
+    }
+
+    class KolokwiumzZadanie1 : KolokwiumzZadaniami
+    {
+        public KolokwiumzZadanie1(Kolokwium kolokwium)
+            : base(kolokwium)
+        { }
+
+        public override decimal Ocena()
+        {
+            return kolokwium.Ocena() + 1M;
+        }
+    }
+
+    class KolokwiumzZadanie2 : KolokwiumzZadaniami
+    {
+        public KolokwiumzZadanie2(Kolokwium kolokwium)
+            : base(kolokwium)
+        { }
+
+        public override decimal Ocena()
+        {
+            return kolokwium.Ocena() + 1M;
+        }
+    }
+    class KolokwiumzZadanie3 : KolokwiumzZadaniami
+    {
+        public KolokwiumzZadanie3(Kolokwium kolokwium)
+            : base(kolokwium)
+        { }
+
+        public override decimal Ocena()
+        {
+            return kolokwium.Ocena() + 1M;
+        }
+    }
+
+
+}
