4f63f3b8212902133df167de31d02b0f33f81e4c
Ivan <35543935+deSantLollipop@users.noreply.github.com>
2020-06-15 12:09:10 +0200

    Add files via upload

---
+++ Zadanie_3/Zadanie_3/BipSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    class BipSingleton
+    {
+        private static BipSingleton instancja;
+
+        private BipSingleton() { } //ukryty konstruktor
+
+        public static BipSingleton Instancja
+        {
+            get
+            {
+                if (instancja == null)
+                    instancja = new BipSingleton();
+                return instancja;
+            }
+        }
+
+        private const string bipString = " BIP BIP ";
+        
+        public void BipBip()
+        {
+            Console.WriteLine(bipString);
+        }
+        
+
+    }
+}
